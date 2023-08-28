c62789218827f1864e749e2b02d0325227f891a4
retajalhogal <142819539+retajalhogal@users.noreply.github.com>
2023-08-28 20:35:59 +0300

    Update main.py
    retaj.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 # write your code here
+favorite_animals = ["dog", "cat", "monkey", "rabbit"]
+print(favorite_animals)
+print(favorite_animals[1])
+favorite_animals.remove('monkey')
+print(favorite_animals)
+favorite_animals.append("elephant")
+print(favorite_animals)
+
+for favorite in favorite_animals:
+    print(f"i like {favorite}")
+
+
+numbers = [1,2,3,4,5]
+
+numbers_sum = 0
+
+for num in numbers: 
+    numbers_sum += num
+    
+print(numbers_sum)
+
+     
+
+
